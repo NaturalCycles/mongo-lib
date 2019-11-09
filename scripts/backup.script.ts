58fb85832fc9760de2fee3614b5d4ddd0fc08bda
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2019-11-09 04:23:28 +0100

    feat: backup.script.ts

---
+++ scripts/backup.script.ts
@@ -0,0 +1,30 @@
+/*
+
+DEBUG=nc* yarn tsn ./scripts/backup.script.ts
+
+ */
+
+import { dbPipelineBackup } from '@naturalcycles/db-lib'
+import { requireEnvKeys, runScript } from '@naturalcycles/nodejs-lib'
+import { MongoDB } from '../src'
+import { tmpDir } from '../src/test/paths.cnst'
+
+const { MONGO_URI } = requireEnvKeys('MONGO_URI')
+
+const mongoDB = new MongoDB({
+  uri: MONGO_URI,
+  db: 'db1',
+})
+
+runScript(async () => {
+  const limit = 0
+  const concurrency = 16
+
+  await dbPipelineBackup({
+    db: mongoDB,
+    outputDirPath: `${tmpDir}/backup`,
+    concurrency,
+    limit,
+    // errorMode,
+  })
+})
